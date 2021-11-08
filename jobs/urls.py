9790518e37e29b1c16eceecbf184a8836938a122
sraikwar1994 <sraikwar@bestpeers.com>
2021-11-08 18:13:56 +0530

    update: urls name

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('create', CreateJobView.as_view(), name="create_job_view"),
-    path('get_detail/<int:pk>', JobDetailView.as_view(), name="job_detail_view"),
-    path('get_list', JobListView.as_view(), name="job_list_view"),
-    path('get_skill', SkillsListView.as_view(), name="skill_list_view"),
+    path('get_job_details/<int:pk>', JobDetailView.as_view(), name="job_detail_view"),
+    path('get_jobs_list', JobListView.as_view(), name="job_list_view"),
+    path('get_skills_list', SkillsListView.as_view(), name="skill_list_view"),
 ]
