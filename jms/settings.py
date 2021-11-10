71c33f2a3f0449c69a8e7aebcb2a49e98dfdb8fb
Akshay Babar <akshaybabar2408@gmail.com>
2021-11-10 22:07:55 +0530

    static file

--- jms/settings.py
+++ jms/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'build/static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
